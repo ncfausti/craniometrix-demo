050e19e52804ba3c4448ca833db1cae4df7ec1ba
Nick Fausti <ncfausti@gmail.com>
2023-03-08 22:05:07 -0500

    Add win condition check

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-
+import "./Board.css";
 // You should display the current player’s turn
 // - Only one disc can be dropped in each turn
 // - For each turn, a player should select a column and their disc should drop to the
 // next empty spot in that column
+
 // - Players must connect 4 of their colored discs in a row to win (either horizontally,
 // vertically, or diagonally)
+
 // - The game ends when there is a 4-in-a-row, or if all spots are occupied by a disc
 // (stalemate)
+
 // - When the game ends, you should stop receiving input from either user, display a
 // message indicating who won the game (or that it was a stalemate game), and be
 // able to start a new game
@@ -17,7 +20,7 @@ const ConnectFour = (props: { board: string[][] }) => {
 
   const renderBoard = () => {
     return board.map((row, rowIndex) => (
-      <div key={rowIndex}>
+      <div className='Board' key={rowIndex}>
         {row.map((cell, columnIndex) => (
           <span key={`${rowIndex}-${columnIndex}`} className='cell'>
             {board[rowIndex][columnIndex] === null
@@ -38,6 +41,66 @@ export default function App(props: any) {
   const BOARD_HEIGHT = 6;
   const BOARD_WIDTH = 7;
 
+  // check for horizontal wins
+  const checkHorizontal = (player: string, board: string[][]) => {
+    for (let row = 0; row < board.length; row++) {
+      for (let col = 0; col <= board[row].length - 4; col++) {
+        if (
+          board[row][col] === player &&
+          board[row][col + 1] === player &&
+          board[row][col + 2] === player &&
+          board[row][col + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  // check for vertical wins
+  const checkVertical = (player: string, board: string[][]) => {
+    for (let row = 0; row <= board.length - 4; row++) {
+      for (let col = 0; col < board[row].length; col++) {
+        if (
+          board[row][col] === player &&
+          board[row + 1][col] === player &&
+          board[row + 2][col] === player &&
+          board[row + 3][col] === player
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  // check for diagonal wins (bottom-left to top-right)
+  const checkDiagonal = (player: string, board: string[][]) => {
+    for (let row = 0; row <= board.length - 4; row++) {
+      for (let col = 0; col <= board[row].length - 4; col++) {
+        if (
+          board[row][col] === player &&
+          board[row + 1][col + 1] === player &&
+          board[row + 2][col + 2] === player &&
+          board[row + 3][col + 3] === player
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const checkWinner = (player: string, board: string[][]) => {
+    if (checkHorizontal(player, board)) return true;
+    if (checkVertical(player, board)) return true;
+    if (checkDiagonal(player, board)) return true;
+
+    // if no winning combinations are found, return false
+    return false;
+  };
+
   // use dropDisc to return a new game board state
   const dropDisc = (column: number, player: string, board: string[][]) => {
     // start at the bottom of the column and search for the first empty slot
@@ -45,6 +108,8 @@ export default function App(props: any) {
       if (board[row][column] === null) {
         // found an empty slot, drop the disc in this position
         board[row][column] = player;
+
+        console.log(`Winner? ${checkWinner(player, board)}`);
         return board; // return the row number where the disc was placed
       }
     }
@@ -67,9 +132,11 @@ export default function App(props: any) {
     const updatedGameState = { ...gameState };
     const currentTurn = updatedGameState.turn;
 
+    // Update the game state by dropping a disc
     updatedGameState.board = dropDisc(0, currentTurn, gameState.board);
+
     // Update who's turn it is
-    updatedGameState.turn = gameState.turn === RED ? YELLOW : RED;
+    // updatedGameState.turn = gameState.turn === RED ? YELLOW : RED;
 
     // Set new gameState
     setGameState(updatedGameState);
@@ -78,10 +145,12 @@ export default function App(props: any) {
   return (
     <div className='App'>
       connect-four
-      <p>Player's turn: {gameState.turn}</p>
+      <p className='Board'>Player's turn: {gameState.turn}</p>
       <p>Winner: {gameState.winner}</p>
       <button onClick={handleClick}>swap turn</button>
-      <ConnectFour board={gameState.board} />
+      <div className='Board'>
+        <ConnectFour board={gameState.board} />
+      </div>
     </div>
   );
 }
